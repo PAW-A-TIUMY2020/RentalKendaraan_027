1e4eabc36ae82b3025157ade123ca8fba2fbea67
Auliyauswatunkhasanah <auliyauswatunn@gmail.com>
2020-10-24 12:54:56 +0700

    Add project files.

---
+++ RentalKendaraan_20180140027/Models/Pengembalian.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_20180140027.Models
+{
+    public partial class Pengembalian
+    {
+        public int IdPengembalian { get; set; }
+        public DateTime? TglPengembalian { get; set; }
+        public int? IdPeminjaaman { get; set; }
+        public int? IdKondisi { get; set; }
+        public int? Denda { get; set; }
+    }
+}
